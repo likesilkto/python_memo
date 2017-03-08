8a606c0ba228705c9d4870ba59c86a562f43adf7
Masayuki Tanaka <mastnk@gmail.com>
2017-03-08 10:48:41 +0900

    crypt100

--- crypt.py
+++ crypt.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import codecs
-import Crypto.Cipher.AES as AES # % pip3 install pycrypto
-from getpass import getpass
+# % pip3 install Simple-AES-Cipher
+# http://qiita.com/teitei_tk/items/0b8bae99a8700452b718
+from simple_aes_cipher import AESCipher, generate_secret_key
 
+passphrase = 'Hello world!'
+cipher = AESCipher(generate_secret_key(passphrase))
+
+message = 'I love python.'
+enc = cipher.encrypt( message )
+dec = cipher.decrypt( enc )
+
+print( enc )
+print( dec )
