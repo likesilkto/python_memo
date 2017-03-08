2e76ed89368701752d32a338e8157e973264d5cf
Masayuki Tanaka <mastnk@gmail.com>
2017-03-08 14:41:54 +0900

    timedelta2str

---
+++ timedelta2str.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+def timedelta2str( td, format='{year:04d}/{month:02d}/{day:02d} {hour:02d}:{minute:02d}:{second:02d}' ):
+	seconds = int(td.total_seconds())
+	periods = [
+		('year',   60*60*24*365),
+		('month',  60*60*24*30),
+		('day',    60*60*24),
+		('hour',   60*60),
+		('minute', 60),
+		('second', 1)
+	]
+	
+	data = {}
+	for period_name, period_seconds in periods:
+		if( seconds > period_seconds ):
+			period_value, seconds = divmod(seconds,period_seconds)
+			data[period_name] = period_value
+		else:
+			data[period_name] = 0
+	
+	str = format.format(
+		year   = data['year'],
+		month  = data['month'],
+		day    = data['day'],
+		hour   = data['hour'],
+		minute = data['minute'],
+		second = data['second'],
+		)
+	return str
+
+if( __name__ == '__main__' ):
+	first_time = datetime(2016,3,4,5,6, 7)
+	last_time =  datetime(2017,3,4,8,9,10)
+	td = last_time - first_time
+	print( timedelta2str( td ) )
+
+# http://stackoverflow.com/questions/538666/python-format-timedelta-to-string
