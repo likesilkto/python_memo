ef80eafe513c7269b364443d6ef602ad6b8c20d3
Masayuki Tanaka <mastnk@gmail.com>
2017-03-09 09:58:17 +0900

    gmailsend_200

--- gmailsend.py
+++ gmailsend.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from simple_aes_cipher import AESCipher, generate_secret_key
+import os
+import os.path
+from stat import *
+import smtplib
 from getpass import getpass
 
-import os.path
-import datetime
-import smtplib
+from simple_aes_cipher import AESCipher, generate_secret_key
 
 from email import encoders
 from email.mime.text import MIMEText
@@ -14,22 +15,15 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from mimetypes import guess_type
 
-import os
-from stat import *
-
-passphrase = "ig%zl:8v!7GgiQ,]v'*r9vb*[k!&`94NU3[e'!UvT1?1L:tj$l:,~.$@)&^Fl*I6"
-cipher = AESCipher(generate_secret_key(passphrase))
 
 class gmailsend(object):
 
-	def __init__(self, passfile = '.gmailsend', relativepath = True, host = 'smtp.gmail.com', port = 465 ):
-		if( relativepath ):
-			passfile = os.path.dirname(__file__) + '/' + passfile
-		account, password = gmailsend.get_accountpass(passfile)
-		self.account = account + '@gmail.com'
-		self.password = password
+	def __init__(self, passpharse, passfile = '.gmailsend', relativepath = True ):
+		host, port, account, password = gmailsend.get_passfile(passpharse, passfile, relativepath)
 		self.host = host
 		self.port = port
+		self.account = account
+		self.password = password
 		
 		# conection test #################################
 		smtp = smtplib.SMTP_SSL(self.host, self.port)
@@ -75,40 +69,87 @@ class gmailsend(object):
 		smtp.sendmail(from_addr, to_addr, msg.as_string())
 		smtp.quit()
 
-	def gen_passfile( passfile = '.gmailsend' ):
-		account = input('account (without @gmail.com): ')
-		password = getpass('password: ')
-		account = cipher.encrypt( account )
-		password = cipher.encrypt( password )
-		with open(passfile, 'w') as fout:
-			fout.write(account+'\n')
-			fout.write(password+'\n')
-		os.chmod(passfile, S_IRUSR|S_IWUSR )
+	def get_passfile( passphrase, passfile = '.gmailsend', relativepath = True ):
+		if( relativepath ):
+			passfile = os.path.dirname(__file__) + '/' + passfile
 
-	def get_accountpass( passfile = '.gmailsend' ):
 		with open(passfile, 'r') as fin:
+			host = fin.readline()
+			port = fin.readline()
 			account = fin.readline()
 			password = fin.readline()
+
+		cipher = AESCipher(generate_secret_key(passphrase))
+		host = cipher.decrypt( host )
+		port = int( cipher.decrypt( port ) )
 		account = cipher.decrypt( account )
 		password = cipher.decrypt( password )
-		return account, password
+		return host, port, account, password
 
+	def gen_passfile( passfile = '.gmailsend', relativepath = True ):
+		if( relativepath ):
+			passfile = os.path.dirname(__file__) + '/' + passfile
+
+		print('''
+passfile includes all infromation to access your mail account.
+PLEASE BE AWARE THE RISK!
+
+If you understand the risk, please type "yes" and enter.
+''')
+		understood = input()
+		if( understood.lower() == 'yes' ):
+			print('''
+*******************************************************
+passphrase will be appeared in plain text in your code.
+*******************************************************
+''')
+
+			passphrase = input( 'passphrase: ' )
+			
+			host = input( 'smtp host (defult: smtp.gmail.com): ' )
+			if( host == '' ):
+				host = 'smtp.gmail.com'
+			
+			port = input( 'port (defult: 465): ' )
+			if( port == '' ):
+				port = '465'
+			account = input('account: ')
+			
+			flg = True
+			while( flg ):
+				password = getpass('password: ')
+				password_conf = getpass('password (confirm): ')
+				if( password == password_conf ):
+					flg = False
+				else:
+					print('Not matched...')
+			
+			cipher = AESCipher(generate_secret_key(passphrase))
+			
+			host = cipher.encrypt( host )
+			port = cipher.encrypt( port )
+			account = cipher.encrypt( account )
+			password = cipher.encrypt( password )
+			with open(passfile, 'w') as fout:
+				fout.write(host+'\n')
+				fout.write(port+'\n')
+				fout.write(account+'\n')
+				fout.write(password+'\n')
+			os.chmod(passfile, S_IRUSR|S_IWUSR )
 
 
 if( __name__ == '__main__'):
 	gmailsend.gen_passfile()
 
-	account, password =  gmailsend.get_accountpass()
-	print(account)
-	print(password)
-
+	passphrase = input('passphrase: ')
+	gmail = gmailsend(passphrase)
+	
 	subject = 'subject'
 	body = 'body'
 	to_addr = ''
 
 	attach_filepath = ['gmailsend.py']
 	
-	gmail = gmailsend()
 	gmail.send(subject, body, to_addr, attach_filepath=attach_filepath )
 
 # http://qiita.com/tadaken3/items/d39a16486c3a89ffa709
